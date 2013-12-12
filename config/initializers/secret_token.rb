ba4aa3fccaddd2196ff923203603cb012d3e11b8
E-Max <max.zab87@gmail.com>
2013-12-12 10:48:16 +0400

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExampleAppLocalization::Application.config.secret_token = '29c15261fd37b6d55e488e3f46a8d60927058ea5d8e543f903d4ac548b63048bfb86a03267aeb972426ada60ab0597789f875a00b5aae7b62e737fa22240b217'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+ExampleAppLocalization::Application.routes.draw do
+  root :to => "home#index"
+end
